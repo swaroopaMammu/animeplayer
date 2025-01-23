25e3742788745dbeb8eb740c620097cd67528f5d
swaroopaMammu <swaroopamammu1997@gmail.com>
2025-01-23 09:31:33 +0530

    for dynamically adjust its padding to respect the system bars

--- app/src/main/java/com/example/animeplayer/MainActivity.kt
+++ app/src/main/java/com/example/animeplayer/MainActivity.kt
@@ -19,11 +19,11 @@ class MainActivity : AppCompatActivity() {
         enableEdgeToEdge()
         mainBinding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(mainBinding.root)
-//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-//            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-//            insets
-//        }
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
         setupNavGraph()
     }
 
